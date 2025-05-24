454e91f735ce5e09bcd9a61fa2993563e817e051
Ruben Tenderini <rubentenderini@gmail.com>
2025-05-24 11:26:24 +0200

    feat: implement recipe update

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeDeleteView
 from . import views
 
 app_name = "recipes_app"
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", RecipeListView.as_view(), name="index"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="detail"),
     path("recipe/add/", views.add_recipe, name="add"),
-    path("reicipe/<int:recipe_id>/delete", views.delete_recipe, name="delete"),
+    path("recipe/<int:recipe_id>/update/", views.update_recipe, name="update"),
+    path("recipe/<int:pk>/delete", RecipeDeleteView.as_view(), name="delete"),
 ]
--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,11 +1,12 @@
 from typing import Any
-from django.forms import formset_factory
+from django.forms import formset_factory, inlineformset_factory
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, DeleteView
 from .form import IngredientForm, RecipeForm
-from recipes_app.models import Recipe
+from recipes_app.models import Ingredient, Recipe
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class RecipeListView(ListView):
@@ -23,7 +24,7 @@ class RecipeDetailView(DetailView):
 
 @login_required
 def add_recipe(request):
-    IngredientFormSet = formset_factory(IngredientForm, min_num=1, extra=0)
+    IngredientFormSet = formset_factory(IngredientForm, min_num=1, extra=2)
     recipe_form = RecipeForm(prefix="recipe")
     ingredient_formset = IngredientFormSet(prefix="ingredient")
 
@@ -52,8 +53,48 @@ def add_recipe(request):
         },
     )
 
+
 @login_required
-def delete_recipe(request, recipe_id):
+def update_recipe(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
-    recipe.delete()
-    return redirect("recipes_app:index")
+    recipe_form = RecipeForm(
+        request.POST or None,
+        prefix="recipe",
+        instance=recipe,
+    )
+
+    IngredientFormSet = inlineformset_factory(
+        Recipe,
+        Ingredient,
+        fields=["name", "quantity"],
+        extra=0,
+        can_delete=True,
+    )
+    ingredient_formset = IngredientFormSet(
+        request.POST or None,
+        instance=recipe,
+        prefix="ingredient",
+    )
+
+    if request.method == "POST":
+        if recipe_form.is_valid() and ingredient_formset.is_valid():
+            recipe_form.save()
+            ingredient_formset.save()
+
+            return redirect("recipes_app:detail", recipe.id)
+
+    return render(
+        request,
+        "recipes/add.html",
+        {
+            "recipe_form": recipe_form,
+            "ingredient_formset": ingredient_formset,
+            "update": True,
+        },
+    )
+
+
+class RecipeDeleteView(LoginRequiredMixin, DeleteView):
+    model = Recipe
+    template_name = "recipes/delete.html"
+    success_url = "/"
